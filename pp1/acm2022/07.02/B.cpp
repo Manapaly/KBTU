0ce174ceff9e389ee47bed750a405f161f6d8dae
Manapaly <e_manapaly@kbtu.kz>
2022-02-07 23:36:00 +0600

    jai

---
+++ pp1/acm2022/07.02/B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int k=0;k<n;k++){
+        string s;
+        cin>>s;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') int k = i;
+            if(s[i]==')') int w = i;
+            
+        }
+    }
+}
