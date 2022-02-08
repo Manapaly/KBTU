e3c553b1911bd4ff5dcb4dce4935fb11b81a4564
Manapaly <e_manapaly@kbtu.kz>
2022-02-08 14:35:21 +0600

    lll

--- pp2/Lab2/a.py
+++ pp2/Lab2/a.py
@@ -1,14 +1,11 @@
 n = list(map(int,input().split()))
-ok = False
-for i in range(len(n)):
-    if n[i]==0:
-        cnt = 0
-        for j in range(i):
-            if (n[j] - n[i]) == (i - j) : cnt += 1
-        if cnt == i: ok = True
-        if n[i]==0 and n[i-1] == 1: ok = True
-        if cnt == len(n)-1: ok = False
-    
-     
-if ok == True: print(0)
-else: print(1)
+m = 0
+
+for i in n:
+    for j in range(m, m+n[m]+1):
+        if m + n[m]  < n[j] + j:
+            m = j
+        if m + n[m] + 1 >= len(n):
+            print(1)
+            exit()
+print(0)
